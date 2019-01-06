db096bf52d811fb7adc12033c56b797565598a3c
보규 박 <shaing123@naver.com>
2019-01-06 20:37:29 +0900

    1181 success

--- Stage9/Stage9/SortWord_1181.cpp
+++ Stage9/Stage9/SortWord_1181.cpp
@@ -1,9 +1,42 @@
 //BOJ_SortWord_1181
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
+
+bool cmp(const pair<string, int> &a, const pair<string, int> &b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	else
+	{
+		return a.second < b.second;
+	}
+}
+
 int main()
 {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
 
+	int N;
+	cin >> N;
+	vector<pair<string, int> > v;
+	string tmp;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> tmp;
+		v.push_back(make_pair(tmp, tmp.size()));
+	}
+
+	sort(v.begin(), v.end(), cmp);
+	//중복 제거
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	int len = v.size();
+	for (int i = 0; i < len; i++)
+	{
+		cout << v[i].first << "\n";
+	}
+	
 	return 0;
 }
