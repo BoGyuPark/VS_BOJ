73f82029dd1e6a4e4ed95a7af917695bb22adab7
보규 박 <shaing123@naver.com>
2019-01-09 23:43:34 +0900

    Stack 도움 받은 문제

--- Stage11/Stage11/ValueOfBracket_2504.cpp
+++ Stage11/Stage11/ValueOfBracket_2504.cpp
@@ -1,22 +1,61 @@
 //BOJ_ValueOfBracket_2504
+//스스로 풀지 못한 문제
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	char arr[31] = { 0, };
+	char arr[33] = { 0, };
 	cin >> arr;
 	int len = strlen(arr);
 	stack<char> s;
+	int val = 0, temp = 1, no = 1;
 	for (int i = 0; i < len; i++)
 	{
-		
-
-
-
-
-
+		if (arr[i] == '(')
+		{
+			s.push(arr[i]);
+			temp *= 2;
+		}
+		else if (arr[i] == '[')
+		{
+			s.push(arr[i]);
+			temp *= 3;
+		}
+		else if (arr[i] == ')')
+		{
+			if (arr[i - 1] == '(')
+				val += temp;
+			if (s.empty() == 1)
+			{
+				no = 0;
+				break;
+			}
+			if (s.top() == '(')
+			{
+				s.pop();
+			}
+			temp /= 2;
+		}
+		else
+		{
+			if (arr[i - 1] == '[')
+				val += temp;
+			if (s.empty() == 1)
+			{
+				no = 0;
+				break;
+			}
+			if (s.top() == '[')
+			{
+				s.pop();
+			}
+			temp /= 3;
+		}
 	}
-
-
+	if (no == 0 || !s.empty())
+		cout << 0;
+	else
+		cout << val;
+	
 	return 0;
 }
