9bda05466f67f42717e81c06f312a36dacfc60da
Bogyu Park <shaing123@naver.com>
2019-08-18 01:05:04 +0900

    BOJ_0817

--- 2019_08/2019_08/BOJ_16925.cpp
+++ 2019_08/2019_08/BOJ_16925.cpp
@@ -2,38 +2,48 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<algorithm>
 using namespace std;
-int n, check[21];
-vector<string> v,one,nMinusOne;
+int n, check[202];
+vector<string> v, li,ansli;
+string s, t;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> n;
 	for (int i = 0; i < 2 * n - 2; i++) {
-		string s;
-		cin >> s;
-		v.push_back(s);
+		cin >> t;
+		v.push_back(t);
 	}
-	string s;
+
 	if (v.size() == 2) {
 		s = v[0] + v[1];
 		cout << s << '\n' << "PS";
 	}
 	else {
-		//접두사 n-1문자열과 접미사 1문자열을 합치자.
+		//접두사 n-1문자열 두개로 원본을 찾아보자
 		for (int i = 0; i < v.size(); i++) {
-			if (v[i].size() == n - 1) nMinusOne.push_back(v[i]);
-			else if (v[i].size() == 1) one.push_back(v[i]);
+			if (v[i].size() == n - 1) li.push_back(v[i]);
 		}
 
-		if (nMinusOne[0] + one[0] == one[1] + nMinusOne[1]) {
-			s = nMinusOne[0] + one[0];
+		//P의 1~end, S의 0~end-1 비교
+		ansli.push_back(li[0] + li[1][li[1].size() - 1]);
+		ansli.push_back(li[1] + li[0][li[0].size() - 1]);
+
+		//find origin
+		int cnt = 0;
+		for (int i = 0; i < ansli.size(); i++) {
+			cnt = 0;
+			for (int j = 0; j < v.size(); j++) {
+				if (ansli[i].substr(0, v[j].size()) == v[j] || 
+					ansli[i].substr(n - v[j].size()) == v[j]) {
+					cnt++;
+				}
+			}
+			if (cnt == 2 * n - 2) {
+				s = ansli[i];
+				break;
+			}
 		}
 
-		if (nMinusOne[0] + one[1] == one[0] + nMinusOne[1]) {
-			s = nMinusOne[0] + one[1];
-		}
-		
 		cout << s << '\n';
 		//find prefic, suffix
 		for (int i = 0; i < v.size(); i++) {
--- 2019_08/2019_08/BOJ_16945.cpp
+++ 2019_08/2019_08/BOJ_16945.cpp
@@ -1,2 +1,52 @@
 /*BOJ 16945 매직 스퀘어로 변경하기*/
- 
+#include<iostream>
+using namespace std;
+int map[9], sel[9], check[9];
+const int n = 9;
+int ans = 2147483647;
+bool findRow() {
+	int t1, t2, t3;
+	t1 = sel[0] + sel[1] + sel[2];
+	t2 = sel[3] + sel[4] + sel[5];
+	t3 = sel[6] + sel[7] + sel[8];
+	if (t1 == 15 && t2 == 15 && t3 == 15) return true;
+	return false;
+}
+bool findCol() {
+	int t1, t2, t3;
+	t1 = sel[0] + sel[3] + sel[6];
+	t2 = sel[1] + sel[4] + sel[7];
+	t3 = sel[2] + sel[5] + sel[8];
+	if (t1 == 15 && t2 == 15 && t3 == 15) return true;
+	return false;
+}
+bool findDia() {
+	int t1, t2;
+	t1 = sel[0] + sel[4] + sel[8];
+	t2 = sel[2] + sel[4] + sel[6];
+	if (t1 == 15 && t2 == 15) return true;
+	return false;
+}
+void go(int index) {
+	if (index == n) {
+		if (findRow() && findCol() && findDia()) {
+			int sum = 0;
+			for (int j = 0; j < n; j++) sum += abs(map[j] - sel[j]);
+			if (ans > sum) ans = sum;
+		}
+		return;
+	}
+	for (int i = 0; i < 9; i++) {
+		if (check[i]) continue;
+		check[i] = true;
+		sel[index] = i + 1;
+		go(index + 1);
+		check[i] = false;
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	for (int i = 0; i < n; i++) cin >> map[i];
+	go(0);
+	cout << ans;
+}
